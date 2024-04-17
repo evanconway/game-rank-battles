4734d460de4e944f945049c9e622947cb97402f1
Evan Conway <evanconwaymusic@gmail.com>
2024-04-17 13:48:11 -0400

    add rank styling

--- client/src/components/Ranks.tsx
+++ client/src/components/Ranks.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BRAND_COLOR } from "../styles";
 
 interface Rank {
   coverUrl: string;
@@ -18,14 +19,42 @@ const Ranks = () => {
   }, [setRanks]);
 
   return (
-    <ul>
+    <ul
+      style={{
+        listStyle: "none",
+        background: BRAND_COLOR,
+        margin: 0,
+        padding: 0,
+      }}
+    >
       {ranks.map((rank, i) => (
-        <li key={i}>
-          <div>#{i + 1}</div>
-          <img src={rank.coverUrl} />
+        <li
+          key={i}
+          style={{
+            color: "white",
+            textAlign: "center",
+            fontSize: "1.25em",
+            paddingBottom: "0.5em",
+          }}
+        >
+          <div
+            style={{
+              background: "black",
+              fontSize: "1.5em",
+              padding: "0.25em",
+            }}
+          >
+            #{i + 1}
+          </div>
+          <img
+            style={{ width: "100%", maxWidth: "400px" }}
+            src={rank.coverUrl}
+          />
           <div>{rank.name}</div>
-          <div>{rank.rank}</div>
-          <a href={rank.igdbUrl}>Learn More</a>
+          <div>{Math.floor(rank.rank)}</div>
+          <a target="_blank" style={{ color: "white" }} href={rank.igdbUrl}>
+            Learn More
+          </a>
         </li>
       ))}
     </ul>
