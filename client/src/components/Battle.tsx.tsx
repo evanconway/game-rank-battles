99f04951d2ec4813a67398f5a52d55288cb70bdd
Evan Conway <43253820+evanconway@users.noreply.github.com>
2024-04-18 00:48:59 -0400

    add previous battle info

--- client/src/components/Battle.tsx.tsx
+++ client/src/components/Battle.tsx.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useMemo, useState } from "react";
 import Game from "./Game";
 import GameDescription from "./GameDescription";
+import { BRAND_COLOR } from "../styles";
 
 interface GameData {
   readonly id: number;
@@ -32,13 +33,13 @@ const Battle = () => {
     fetchBattle();
   }, [fetchBattle, battleData]);
 
-  if (battleData === null)
-    return <div style={{ textAlign: "center" }}>fetching battle data...</div>;
-
-  const gameA = battleData["gameA"] as GameData;
-  const gameB = battleData["gameB"] as GameData;
+  const [prevBattle, setPrevBattle] = useState<{
+    victor: GameData;
+    loser: GameData;
+  } | null>(null);
 
   const submitVictor = async (victor: GameData, loser: GameData) => {
+    if (battleData === null) return;
     setUploading(true);
     await fetch("/app/battle/end", {
       method: "POST",
@@ -51,12 +52,70 @@ const Battle = () => {
         loserId: loser.id,
       }),
     });
+    setPrevBattle({ victor, loser });
     await fetchBattle();
     setUploading(false);
   };
 
+  /*
+  Be aware that all hooks must be above this return line. The same number
+  of hooks must be called each render cycle. If a hook is below this line
+  the app will crash.
+  */
+  if (battleData === null)
+    return (
+      <div
+        style={{ textAlign: "center", background: BRAND_COLOR, color: "white" }}
+      >
+        fetching battle data...
+      </div>
+    );
+
+  const gameA = battleData["gameA"] as GameData;
+  const gameB = battleData["gameB"] as GameData;
+
   return (
     <div>
+      {prevBattle === null ? (
+        <div
+          style={{
+            background: "black",
+            color: "white",
+            textAlign: "center",
+            padding: "0.5em",
+            fontSize: "1.5em",
+          }}
+        >
+          choose your favorite
+        </div>
+      ) : (
+        <div
+          style={{
+            background: "black",
+            color: "white",
+            textAlign: "center",
+            padding: "0.5em",
+          }}
+        >
+          You chose{" "}
+          <a
+            style={{ color: "white" }}
+            target="_blank"
+            href={prevBattle.victor.igdbUrl}
+          >
+            {prevBattle.victor.name}
+          </a>{" "}
+          over{" "}
+          <a
+            style={{ color: "white" }}
+            target="_blank"
+            href={prevBattle.loser.igdbUrl}
+          >
+            {prevBattle.loser.name}
+          </a>
+          .
+        </div>
+      )}
       <div style={{ display: "flex" }}>
         <Game
           coverUrl={gameA.coverUrl}
